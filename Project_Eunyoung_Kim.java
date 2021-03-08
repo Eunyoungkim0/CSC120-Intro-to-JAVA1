87647b78e0e9eb23472c3a9a23d7572d440a801f
silverzero <ekim0010@email.cpcc.edu>
2021-03-08 12:51:37 -0500

    project1
    Added code for determining performance score and recommended graphics quality

--- Project_Eunyoung_Kim.java
+++ Project_Eunyoung_Kim.java
@@ -8,6 +8,9 @@ public class Project_Eunyoung_Kim
       int cSpeed = 0;
       int cCore  = 0;
       int resolution = 0;
+      double multiplier = 0.0;
+      double performance = 0.0;
+      String gRecommend = "";
       
       //Create a Scanner object to read input
       Scanner input = new Scanner(System.in);
@@ -26,5 +29,38 @@ public class Project_Eunyoung_Kim
                          "3. 2560 x 1440\n" +
                          "4. 3840 x 2160\n");
       resolution = input.nextInt();
+      
+      //Determine multiplier value by the resolution
+      switch(resolution)
+      {
+         case 1:
+            multiplier = 1.0;
+            break;
+         case 2:
+            multiplier = 0.75;
+            break;
+         case 3:
+            multiplier = 0.55;
+            break;
+         case 4:
+            multiplier = 0.35;
+            break;
+         default:
+            multiplier = 0.0;
+      }
+      
+      performance = ((5 * gSpeed) + (cCore * cSpeed)) * multiplier;
+      
+      if(performance > 17000){
+         gRecommend = "Ultra";
+      }else if(performance > 15000){
+         gRecommend = "High";
+      }else if(performance > 13000){
+         gRecommend = "Medium";
+      }else if(performance > 11000){
+         gRecommend = "Low";
+      }else{
+         gRecommend = "Unable to play";
+      }
    }
 }
